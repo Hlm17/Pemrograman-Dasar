08ca67fc7bc408e6379783b975f6ebeaddfeaddb
Hlm17 <mhilmirajwandhika@gmail.com>
2026-06-02 22:30:30 +0700

    cetakTabel

---
+++ cetakTabel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class cetakTabel {
+    static int cetakTabel(int n) {
+        for (int i=1; i<=10; i++) {
+            System.out.println(n + " x " + i + " = " + (n*i));
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka untuk mencetak tabel perkalian: ");
+        int n = sc.nextInt();
+        cetakTabel(n);
+        sc.close();
+    }
+}
