1e3eac3cf0454cac097b9621d477aa0b9d0382d3
Hlm17 <mhilmirajwandhika@gmail.com>
2026-06-04 10:47:08 +0700

    Salah method, harusnya prosedur

--- cetakTabel.java
+++ cetakTabel.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 public class cetakTabel {
-    static int cetakTabel(int n) {
+    static void cetakTabel(int n) {
         for (int i=1; i<=10; i++) {
             System.out.println(n + " x " + i + " = " + (n*i));
         }
-        return n;
     }
 
     public static void main(String[] args) {
